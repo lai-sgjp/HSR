1939bdd7f128196dd961efc78265652bc200ee9e
lai_sgjp <lai_sgjp@hust.edu.cn>
2026-07-29 20:03:03 +0800

    test: require p17 console movement audit

--- Source/HSR/Tests/HSREquipmentDevelopmentHarnessTests.cpp
+++ Source/HSR/Tests/HSREquipmentDevelopmentHarnessTests.cpp
@@ -2,6 +2,7 @@
 #include "Misc/AutomationTest.h"
 #include "../Equipment/HSREquipmentDevelopmentHarness.h"
 #include "../Equipment/HSREquipmentSubsystem.h"
+#include "../Inventory/HSRInventorySubsystem.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FHSREquipmentDevelopmentHarnessTest,"HSR.Equipment.DevelopmentHarness",EAutomationTestFlags::EditorContext|EAutomationTestFlags::EngineFilter)
 bool FHSREquipmentDevelopmentHarnessTest::RunTest(const FString&)
@@ -12,6 +13,7 @@ bool FHSREquipmentDevelopmentHarnessTest::RunTest(const FString&)
 	TestTrue(TEXT("restore second relic"),FHSREquipmentDevelopmentHarness::SetupFixedLoadoutForTest(Equipment));Equipment->GetLoadout(CharacterId,Loadout,Revision);const int32 RestoreRevision=Revision;TestTrue(TEXT("restore revision increases"),RestoreRevision>RemoveRevision);TestEqual(TEXT("two relics restored"),Loadout.Relics.Num(),2);TestEqual(TEXT("weapon id stable"),Loadout.Equipment.FindChecked(EHSREquipmentSlot::Weapon).InstanceId,WeaponId);TestEqual(TEXT("head id stable"),Loadout.Relics.FindChecked(EHSRRelicSlot::Head).InstanceId,HeadId);TestEqual(TEXT("hands id stable"),Loadout.Relics.FindChecked(EHSRRelicSlot::Hands).InstanceId,HandsId);
 	TestTrue(TEXT("clear loadout"),FHSREquipmentDevelopmentHarness::ClearLoadoutForTest(Equipment));TestFalse(TEXT("clear removes authority loadout"),Equipment->GetLoadout(CharacterId,Loadout,Revision));TestTrue(TEXT("setup after clear"),FHSREquipmentDevelopmentHarness::SetupFixedLoadoutForTest(Equipment));Equipment->GetLoadout(CharacterId,Loadout,Revision);TestTrue(TEXT("post-clear setup revision increases"),Revision>RestoreRevision);
 	TestTrue(TEXT("cleanup is idempotent"),FHSREquipmentDevelopmentHarness::CleanupSave());
+	UHSRInventorySubsystem* Inventory=NewObject<UHSRInventorySubsystem>(GI);TestTrue(TEXT("P17 movement audit completes"),FHSREquipmentDevelopmentHarness::RunP17MovementAuditForTest(Equipment,Inventory));
 	return true;
 }
 #endif
